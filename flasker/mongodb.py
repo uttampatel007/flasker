01792c855027d7d08bb250fcab4ff9a2902248e6
uttampatel007 <uttam11.velani11@gmail.com>
2021-07-01 17:48:13 +0530

    docker-setuped

--- flasker/mongodb.py
+++ flasker/mongodb.py
@@ -2,5 +2,5 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myBlogDatabase"
+app.config["MONGO_URI"] = "mongodb://mongodb/myBlogDatabase"
 mongo = PyMongo(app)
